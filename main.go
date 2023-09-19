17e679cebe8768a4782c32cf4dce1faed9f53605
AmelAbema <dogormous14@gmail.com>
2023-09-19 21:35:04 +0200

    6.1

--- main.go
+++ main.go
@@ -31,7 +31,7 @@ var staticFiles embed.FS
 func main() {
 	err := godotenv.Load(".env")
 	if err != nil {
-		log.Fatalf("Error loading .env file: %v", err)
+		log.Printf("warning: Error loading .env file: %v", err)
 	}
 
 	port := os.Getenv("PORT")
